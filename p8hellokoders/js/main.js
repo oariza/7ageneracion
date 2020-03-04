4919909e054ab10662a81f0c4bb9008c19ff7681
Omar Ariza <arizaomar@gmail.com>
2020-03-03 23:50:00 -0600

    [ADD] index.html and main.js files

---
+++ p8hellokoders/js/main.js
@@ -0,0 +1,9 @@
+var x = 5
+var y = 3
+
+console.log("La suma de x,y es igual a", x+y)
+console.log("La resta de x,y es igual a", x-y)
+console.log("La multiplicación de x,y es igual a", x*y)
+console.log("La división de x,y es igual a", x/y)
+
+
